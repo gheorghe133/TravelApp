a1c5fb7836c20478799b6a6705933036e97d1718
gheorghe133 <aaw1812.dandis.gheorghe@gmail.com>
2024-03-12 12:41:00 +0200

    Added product list and model

--- TravelApplication/Controllers/HomeController.cs
+++ TravelApplication/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelApplication.Models;
 
 namespace TravelApplication.Controllers
 {
@@ -10,7 +11,29 @@ namespace TravelApplication.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = new List<Product>
+        {
+            new Product { 
+                Title = "Amazon Cruise", 
+                Description = "O călătorie incredibilă pe Amazon, explorând frumusețea naturii și a vieții sălbatice.",
+                Photo = "/assets/images/gallery/gallery_01.jpg",
+                Price = 1999.99 
+            },
+            new Product { 
+                Title = "Egipt Expedition", 
+                Description = "Descoperiți secretele istoriei și cultura Egiptului într-o aventură plină de mister și minuni arheologice.",
+                Photo = "/assets/images/gallery/gallery_02.jpg", 
+                Price = 2999.99 
+            },
+            new Product { 
+                Title = "Mountain Retreat",
+                Description = "Relaxați-vă în mijlocul naturii, respirați aer proaspăt de munte și bucurați-vă de priveliști spectaculoase.", 
+                Photo = "/assets/images/gallery/gallery_03.jpg", 
+                Price = 1599.99 
+            },
+        };
+
+            return View(products);
         }
 
         public ActionResult About()
---
+++ TravelApplication/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelApplication.Models
+{
+    public class Product
+    {
+        public string Title { get; set; }
+        
+        public string Description { get; set; }
+
+        public string Photo { get; set; }
+
+        public double Price { get; set; }
+    }
+}
